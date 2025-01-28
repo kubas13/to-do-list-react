ca8061b6985aa8ab8cab4df0298363229e901df9
Kamil Kubas <kamilkubas13@gmail.com>
2025-01-28 15:44:10 +0100

    adding item to list

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,44 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [...currentTodos,
+         {id: crypto.randomUUID(), title: newItem,
+           completed: false}, 
+      ]
+    })
+  }
+
   return (
   <>
-  <form className="new-item-form">
+  <form onSubmit = {handleSubmit} className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
-      <input type="text" id="item"></input>
+      <input value={newItem}
+      onChange ={e => setNewItem(e.target.value)} 
+      type="text" 
+      id="item"></input>
     </div>
     <button className="btn">Add</button>
   </form>
   <h1 className="header">TodoList</h1>
   <ul className="list">
-    <li>
+    {todos.map(todo => {
+      return <li>
       <label>
-        <input type="checkbox" />
-        Item 1
+        <input type="checkbox" checked = {todo.completed} />
+        {todo.title}
       </label>
       <button className="btn btn-danger">Delete</button>
     </li>
-    <li>
-      <label>
-        <input type="checkbox" />
-        Item 2
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
+    })}
   </ul>
   </>
   )
