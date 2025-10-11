de359b77aa0e2907c316cf2d3492791f36775c54
kubas13 <kamilkubas13@gmail.com>
2025-10-11 03:09:54 +0200

    added callendar view

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import "./styles.css";
 import { NewTodoForm } from "./components/NewTodoForm";
 import { TodoList } from "./components/ToDoList";
 import Modal from "./components/CustomModal";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { TodoItem } from "./components/TodoItem";
+
+const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
 
 export default function App() {
   const [todos, setTodos] = useState(() => {
@@ -19,6 +29,8 @@ export default function App() {
   const [showDeleteAllModal, setShowDeleteAllModal] = useState(false);
   const [lastDeleted, setLastDeleted] = useState(null);
   const [showUndo, setShowUndo] = useState(false);
+  const [view, setView] = useState("list");
+  const [selectDate, setSelectDate] = useState(null);
 
   let undoTimeoutRef = null;
 
@@ -31,7 +43,6 @@ export default function App() {
         date,
         time,
         note,
-        priority,
         completed: false,
       },
     ]);
@@ -141,40 +152,107 @@ export default function App() {
       )}
 
       <h1 className="header">To Do List</h1>
-      <NewTodoForm onSubmit={addTodo} />
-      <div className="controls">
-        <div className="filter-buttons">
-          <button onClick={() => setFilter("all")}>All tasks</button>
-          <button onClick={() => setFilter("active")}>Not finished</button>
-          <button onClick={() => setFilter("completed")}>Finished</button>
-        </div>
 
-        <div className="sort-buttons">
-          <button onClick={() => setSort("newest")}>Newest</button>
-          <button onClick={() => setSort("oldest")}>Oldest</button>
-          <button onClick={() => setSort("a-z")}>A-Z</button>
-          <button onClick={() => setSort("z-a")}>Z-A</button>
-        </div>
-        <div className="clear-buttons">
-          <button onClick={handleDeleteCompletedClick}>Delete completed</button>
-          <button onClick={handleDeleteAllClick}>Delete all</button>
-        </div>
+      <div className="view-toggle">
+        <button
+          onClick={() => setView("list")}
+          className={view === "list" ? "active" : ""}
+        >
+          List View
+        </button>
+        <button
+          onClick={() => setView("calendar")}
+          className={view === "calendar" ? "active" : ""}
+        >
+          Calendar view
+        </button>
+
+        <div className="(`view-toggle-indicator ${view}`"></div>
       </div>
-      <TodoList
-        todos={filteredAndSortedTodos}
-        toggleTodo={toggleTodo}
-        deleteTodo={deleteTodo}
-        updateTodo={updateTodo}
-      />
-      {showDeleteCompleteModal && (
-        <Modal
-          title="Delete all completed tasks?"
-          message="This will remove all completed tasks. Are you sure?"
-          onConfirm={confirmDeleteCompleted}
-          onCancel={() => setShowDeleteCompletedModal(false)}
-        />
-      )}
+      {view === "list" ? (
+        <>
+          <NewTodoForm
+            defaultDate={selectDate}
+            onSubmit={({ title, date, time, note }) => {
+              addTodo({
+                title,
+                date: date ? date : formatDate(selectDate),
+                time,
+                note,
+              });
+            }}
+          />
+          <div className="controls">
+            <div className="filter-buttons">
+              <button onClick={() => setFilter("all")}>All tasks</button>
+              <button onClick={() => setFilter("active")}>Not finished</button>
+              <button onClick={() => setFilter("completed")}>Finished</button>
+            </div>
 
+            <div className="sort-buttons">
+              <button onClick={() => setSort("newest")}>Newest</button>
+              <button onClick={() => setSort("oldest")}>Oldest</button>
+              <button onClick={() => setSort("a-z")}>A-Z</button>
+              <button onClick={() => setSort("z-a")}>Z-A</button>
+            </div>
+            <div className="clear-buttons">
+              <button onClick={handleDeleteCompletedClick}>
+                Delete completed
+              </button>
+              <button onClick={handleDeleteAllClick}>Delete all</button>
+            </div>
+          </div>
+          <TodoList
+            todos={filteredAndSortedTodos}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+            updateTodo={updateTodo}
+          />
+          {showDeleteCompleteModal && (
+            <Modal
+              title="Delete all completed tasks?"
+              message="This will remove all completed tasks. Are you sure?"
+              onConfirm={confirmDeleteCompleted}
+              onCancel={() => setShowDeleteCompletedModal(false)}
+            />
+          )}
+        </>
+      ) : (
+        <div className="calendar-view">
+          <Calendar
+            onChange={setSelectDate}
+            value={selectDate}
+            tileClassName={({ date, view }) => {
+              if (view === "month") {
+                const dayHasTodo = todos.some(
+                  (todo) => todo.date === formatDate(date)
+                );
+                return dayHasTodo ? "has-todo" : null;
+              }
+            }}
+          />
+          <h2>
+            Tasks for:{" "}
+            {selectDate ? selectDate.toLocaleDateString() : "Select a date"}
+          </h2>
+
+          <ul className="calendar-tasks">
+            {todos
+              .filter(
+                (todo) => selectDate && todo.date === formatDate(selectDate)
+              )
+              .map((todo) => (
+                <TodoItem
+                  key={todo.id}
+                  {...todo}
+                  toggleTodo={toggleTodo}
+                  deleteTodo={deleteTodo}
+                  updateTodo={updateTodo}
+                />
+              ))}
+          </ul>
+        </div>
+      )}
       {showDeleteAllModal && (
         <Modal
           title="Delete all tasks?"
